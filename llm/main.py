3f47e159fd68bf7bba1b9f12428a6bb052c08e42
paul-nguyen96 <paul121596@icloud.com>
2025-04-07 14:56:57 -0500

    ingest either image or messages on post request

--- llm/main.py
+++ llm/main.py
@@ -1,19 +1,40 @@
 from fastapi import FastAPI, Request
 import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import HumanMessage
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 app = FastAPI()
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL = os.getenv("GEMINI_MODEL")
+genai.configure(api_key=GEMINI_API_KEY)
 
 @app.post("/")
 async def generate_message(request: Request):
     data = await request.json()
+    
     user_message = data.get("message", "")
+    image_url = data.get("image_url")
 
-    model = genai.GenerativeModel(os.getenv("GEMINI_VERSION", "gemini-1.5-flash"))
-    response = model.generate_content(user_message)
+    llm = ChatGoogleGenerativeAI(
+            model="gemini-1.5-flash",
+            google_api_key=GEMINI_API_KEY
+    )
+    if image_url:
+        message = HumanMessage(
+            content=[
+                {"type": "text", "text": user_message},
+                {"type": "image_url", "image_url": image_url}
+            ]
+        )
+    else:
+        message = HumanMessage(
+            content=[
+                {"type": "text", "text": user_message},
+            ]
+        )
 
-    return {"message": response.text}
+    response = llm.invoke([message])
+    return {"message": response.content}
